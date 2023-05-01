998064be9adb9b53d79e915026242696ff7d76b4
radomyr <h.radomyr@proton.me>
2023-05-01 22:39:35 +0300

    feat: Add clear, __getitem__, __setitem__ and __str__

--- grayscale_manipulator.py
+++ grayscale_manipulator.py
@@ -30,3 +30,41 @@ class GrayscaleImage:
             int: image height
         """
         return self._nrows
+
+    def clear(self, value: int = 0) -> None:
+        """Clear image.
+
+        Args:
+            value (int, optional): pixel value to clear image with. Defaults to 0.
+        """
+        self._pixels = [[value for _ in range(self._ncols)] for _ in range(self._nrows)]
+
+    def __getitem__(self, index: tuple) -> int:
+        """Return pixel value at given coordinates.
+
+        Args:
+            index (tuple): coordinates
+
+        Returns:
+            int: pixel value
+        """
+        return self._pixels[index[0]][index[1]]
+
+    def __setitem__(self, index: tuple, value: int) -> None:
+        """Set pixel value at given coordinates.
+
+        Args:
+            index (tuple): coordinates
+            value (int): pixel value
+        """
+        self._pixels[index[0]][index[1]] = value
+
+    def __str__(self) -> str:
+        """Return string representation of image.
+
+        Returns:
+            str: string representation of image
+        """
+        return "\n".join(
+            [" ".join([str(pixel) for pixel in row]) for row in self._pixels]
+        )

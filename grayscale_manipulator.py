b9e5fc765702b463001c73b3ef12c232f82afe97
radomyr <h.radomyr@proton.me>
2023-05-01 22:40:17 +0300

    feat: add from_file and to_file methods

--- grayscale_manipulator.py
+++ grayscale_manipulator.py
@@ -1,4 +1,8 @@
 """Grayscale image manipulator."""
+from __future__ import annotations
+
+import numpy as np
+from PIL import Image, ImageOps
 
 
 class GrayscaleImage:
@@ -68,3 +72,30 @@ class GrayscaleImage:
         return "\n".join(
             [" ".join([str(pixel) for pixel in row]) for row in self._pixels]
         )
+
+    @staticmethod
+    def from_file(path: str) -> GrayscaleImage:
+        """Read image from file in png or jpeg format.
+
+        Args:
+            path (str): path to image file
+
+        Returns:
+            GrayscaleImage: grayscale image
+        """
+        image = np.array(ImageOps.grayscale(Image.open(path)))
+        nrows, ncols = image.shape
+        grayscale_image = GrayscaleImage(nrows, ncols)
+        for row in range(nrows):
+            for col in range(ncols):
+                grayscale_image[row, col] = image[row, col]
+        return grayscale_image
+
+    def to_file(self, path: str) -> None:
+        """Write image to file in png format.
+
+        Args:
+            path (str): path to image file
+        """
+        image = np.array(self._pixels, dtype=np.uint8)
+        Image.fromarray(image).save(path)

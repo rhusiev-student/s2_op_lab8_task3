63572e4d75d3ceb57d44b97eb3457bdeb3fec62a
radomyr <h.radomyr@proton.me>
2023-05-02 13:53:44 +0300

    feat: complete quality.py

--- quality.py
+++ quality.py
@@ -1,15 +1,48 @@
 """Check the quality of the lzw compression."""
 from grayscale_manipulator import GrayscaleImage
+import timeit
+import os
 
 
 def check_quality():
     """Check the quality of the lzw compression."""
-    chess_img = GrayscaleImage.from_file('chess.png')
-    chess_img.lzw_compression('chess_compressed')
-    chess_img = chess_img.lzw_decompression('chess_grayscale.png')
-    chess_img.lzw.save_pickle('chess_raw.pickle')
+    print("Chess image:")
+    original_size = os.path.getsize("chess.png")
+    print("    Original size:", original_size)
+    chess_img = GrayscaleImage.from_file("chess.png")
 
-    mountain_img = GrayscaleImage.from_file('mountain.jpg')
-    mountain_img.lzw_compression('mountain_compressed')
-    mountain_img = mountain_img.lzw_decompression('mountain_grayscale.png')
-    mountain_img.lzw.save_pickle('mountain_raw.pickle')
+    start = timeit.default_timer()
+    chess_img.lzw_compression("chess_compressed")
+    stop = timeit.default_timer()
+    print("    Compression time: ", stop - start)
+    compressed_size = os.path.getsize("chess_compressed.pickle")
+    print(f"    Compressed size: {compressed_size}")
+
+    start = timeit.default_timer()
+    chess_img = chess_img.lzw_decompression("chess_grayscale.png")
+    stop = timeit.default_timer()
+    print("    Decompression time: ", stop - start)
+
+    chess_img.lzw.save_pickle("chess_raw.pickle")
+    print("    Raw size: ", os.path.getsize("chess_raw.pickle"))
+
+    print()
+    print("Mountain image:")
+    original_size = os.path.getsize("mountain.jpg")
+    print("    Original size:", original_size)
+    mountain_img = GrayscaleImage.from_file("mountain.jpg")
+
+    start = timeit.default_timer()
+    mountain_img.lzw_compression("mountain_compressed")
+    stop = timeit.default_timer()
+    print("    Compression time: ", stop - start)
+    compressed_size = os.path.getsize("mountain_compressed.pickle")
+    print(f"    Compressed size: {compressed_size}")
+
+    start = timeit.default_timer()
+    mountain_img = mountain_img.lzw_decompression("mountain_grayscale.png")
+    stop = timeit.default_timer()
+    print("    Decompression time: ", stop - start)
+
+    mountain_img.lzw.save_pickle("mountain_raw.pickle")
+    print("    Raw size: ", os.path.getsize("mountain_raw.pickle"))
